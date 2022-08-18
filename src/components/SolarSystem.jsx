980d9e131458bbfd18cbd15e09aa91488c99eab4
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-18 15:41:21 -0300

    6 concluido

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
+
+const lis = planets.map(({ name, image }) => (
+  <li key={ name }>
+    <PlanetCard planetName={ name } planetImage={ image } />
+  </li>
+));
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <ul>{lis}</ul>
       </div>
     );
   }
