6d558aea8539addbfbe0f1fd1654cf3fa77a04fe
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-18 17:59:49 -0300

    css Titulos prontos

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -14,7 +14,7 @@ class SolarSystem extends React.Component {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
-        <ul>{lis}</ul>
+        <ul className="solar">{lis}</ul>
       </div>
     );
   }
