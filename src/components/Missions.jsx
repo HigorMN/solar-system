e7e306fb6409da1814422a9ec3d65b7e2fe9b161
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-18 16:26:56 -0300

    Missions concluido

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import MissionCard from './MissionCard';
 import Title from './Title';
+import missions from '../data/missions';
+
+const lista = missions.map(({ name, year, country, destination }) => (
+  <li key={ name }>
+    <MissionCard
+      name={ name }
+      year={ year }
+      country={ country }
+      destination={ destination }
+    />
+  </li>
+));
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <ul>{lista}</ul>
       </div>
     );
   }
